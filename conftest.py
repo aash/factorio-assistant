56acaeecea8bcd46859a0150575474fd2b7ce184
Aleksei Shpitalev <aleksei.shpitalev@gmail.com>
2024-03-24 10:54:03 +0700

    make test configs

--- conftest.py
+++ conftest.py
@@ -2,11 +2,6 @@ import logging
 import sys
 
 def pytest_configure(config):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    
-
-def pytest_sessionstart(session):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
@@ -16,4 +11,4 @@ def pytest_sessionstart(session):
         ],
         encoding='utf-8'
     )
-    logging.info("Test session is starting.")
+    logging.info("Pytest configure...")
